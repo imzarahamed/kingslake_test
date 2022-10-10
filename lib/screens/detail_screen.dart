7b1804e2750f481db930c1f96378fa653d0d33da
Imzar Ahamed <55689405+imzarahamed@users.noreply.github.com>
2022-10-10 07:51:00 +0530

    Updated details Screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -23,62 +23,97 @@ class _PersonDetailsState extends State<PersonDetails> {
     final String? halfDay = productdata?.halfDay;
     final String? reason = productdata?.reason;
     // final int? approved = productdata?.approved;
-
+    final ButtonStyle style1 =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+    final ButtonStyle style2 =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
     return Scaffold(
       appBar: AppBar(
         // title: const Text('Approved'),
         title: Text(name ?? "-"),
       ),
-      body: Column(
+      backgroundColor: const Color.fromARGB(77, 246, 245, 245).withOpacity(0.9),
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: const Text("Category"),
-              trailing: Text(category ?? "-"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: const Text("Balance"),
-              trailing: Text("$balance"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: const Text("From Date"),
-              trailing: Text(fromDate ?? "-"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: const Text("To Date"),
-              trailing: Text(toDate ?? "-"),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: ListTile(
-              title: Text("No of Days"),
-              trailing: Text("0.5"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: const Text("Half Day"),
-              trailing: Text(halfDay ?? "-"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: const Text("Reason"),
-              trailing: Text(reason ?? "-"),
-            ),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 15.0, bottom: 2.0),
+                child: ListTile(
+                  title: const Text("Category"),
+                  trailing: Text(category ?? "-"),
+                  tileColor: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, bottom: 20.0),
+                child: ListTile(
+                  title: const Text("Balance"),
+                  trailing: Text("$balance"),
+                  tileColor: Colors.white,
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 15.0, right: 15.0, bottom: 2.0),
+                child: ListTile(
+                  title: const Text("From Date"),
+                  trailing: Text(fromDate ?? "-"),
+                  tileColor: Colors.white,
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 15.0, right: 15.0, bottom: 2.0),
+                child: ListTile(
+                  title: const Text("To Date"),
+                  trailing: Text(toDate ?? "-"),
+                  tileColor: Colors.white,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20.0),
+                child: ListTile(
+                  title: Text("No of Days"),
+                  trailing: Text("0.5"),
+                  tileColor: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, bottom: 20.0),
+                child: ListTile(
+                  title: const Text("Half Day"),
+                  trailing: Text(halfDay ?? "-"),
+                  tileColor: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+                child: ListTile(
+                  title: const Text("Reason"),
+                  trailing: Text(reason ?? "-"),
+                  tileColor: Colors.white,
+                ),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  OutlinedButton(
+                    style: style1,
+                    onPressed: () {},
+                    child: const Text('Reject'),
+                  ),
+                  ElevatedButton(
+                    style: style2,
+                    onPressed: () {},
+                    child: const Text('Approve'),
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
